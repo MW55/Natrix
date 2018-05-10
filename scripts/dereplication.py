002aeb1bb03d0e1c381407d06cd33ae5a5dedbe0
Marius <marius.welzel@stud.uni-due.de>
2018-05-10 17:46:04 +0200

    fixed typo

--- scripts/dereplication.py
+++ scripts/dereplication.py
@@ -1,10 +1,15 @@
 import dinopy
 from glob import glob
-from collections import OrderedDict
 
+# Parsing the whole fasta to a dict, at this point in the pipeline the files
+# are small enugh to fit into memory and using a dict comp with dinopy
+# is quicker then the to_dict func from SeqIO
 seq_dict = {entry.name:entry.sequence for entry in dinopy.FastaReader(snakemake.input).entries()}
 clust = str(snakemake.input)[:-6] + '_cdhit.fasta.clstr'
 
+# returns a list of tuples containing the old header of the representative
+# seq of each cluster and the new header as required by uchime
+# (sorted by cluster size)
 def cluster_size(clust):
     clust_sizes = []
     ids = []
@@ -14,7 +19,7 @@ def cluster_size(clust):
         for line in f:
             if line[0] == '>':
                 if current is not None and count > 0:
-                    clust_sizes.append('{}; size = {};'.format(current[1:].strip().replace('Cluster ', clust.split('/')[-2] + '_'), count))
+                    clust_sizes.append('{};size={};'.format(current[1:].strip().replace('Cluster ', clust.split('/')[-2] + '_'), count))
                 current = line
                 count = 0
             elif line[-2] == '*':
@@ -26,12 +31,8 @@ def cluster_size(clust):
     return list(zip(clust_sizes, ids))
 
 c_size = cluster_size(clust)
+# writes the representatives in a fasta file with the new headers, uses the old header as key
+# to get the matching sequences from the dict
 with dinopy.FastaWriter(str(snakemake.output), force_overwrite=True, line_width = 1000) as clust:
     clust.write_entries([(sequence_dict[line[1].encode()], line[0].encode()) for line in c_size])
     clust.close()
-
-#with dinopy.FastaWriter(str(snakemake.output), line_width=1000) as clust:
-#    for i in range(len(sorted_counter)):
-#        name = '{}_{};size={};'.format(sample_name, i+1, sorted_counter[i][1])
-#        clust.write_entry((sorted_counter[i][0],bytes(name, 'utf-8')))
-#    clust.close()

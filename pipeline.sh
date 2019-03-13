d37afbc9234588d8be30c9f065230e23bed54431
Marius <marius.welzel@gmx.de>
2019-03-13 15:37:51 +0100

    removed hardcoded conda.sh path

--- pipeline.sh
+++ pipeline.sh
@@ -3,7 +3,11 @@
 echo Please enter the name of the project
 
 read varname
+env_loc=$(conda info | grep "active\ env\ location" | awk '{print $5}')
+env_loc+="/etc/profile.d/conda.sh"
+source $env_loc
 #source ~/anaconda3/etc/profile.d/conda.sh
+
 conda activate snakemake
 python demultiplexing.py "$varname"
 
@@ -11,4 +15,6 @@ varname+=".yaml"
 cores=$(grep "cores : " $varname | awk '{print $3}')
 python create_dataframe.py "$varname"
 
-screen -S $varname bash -c "source ~/anaconda3/etc/profile.d/conda.sh;conda activate snakemake;snakemake --use-conda --cores $cores --configfile $varname; exec sh"
+screen -S $varname bash -c "source $env_loc;conda activate snakemake;snakemake --use-conda --cores $cores --configfile $varname; exec sh"
+
+#screen -S $varname bash -c "source ~/anaconda3/etc/profile.d/conda.sh;conda activate snakemake;snakemake --use-conda --cores $cores --configfile $varname; exec sh"

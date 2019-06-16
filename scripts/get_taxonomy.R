f064a5317d0085776a85bce05790ff3aa8e2aedc
MW55 <marius.welzel@gmx.de>
2019-06-16 17:02:06 +0200

    Cleanup

--- scripts/get_taxonomy.R
+++ scripts/get_taxonomy.R
@@ -1,10 +1,9 @@
 library("data.table")
 library("tidyverse")
 
-ncbi_get_tax = function() {
-    ncbi_tax_dir =  dirname(snakemake@params[["db_path"]])
-    blast_table = fread(snakemake@input[[1]])
-    ncbi_tax_ids = c(459536)
+ncbi_get_tax <- function() {
+    ncbi_tax_dir <- dirname(snakemake@params[["db_path"]])
+    blast_table <- fread(snakemake@input[[1]])
     ranked_lineage_file_index  <- grep("rankedlineage.dmp" , list.files(ncbi_tax_dir , full.names = T))
 
     ranked_lineage_file <- list.files(ncbi_tax_dir , full.names = T)[ranked_lineage_file_index]
@@ -15,13 +14,12 @@ ncbi_get_tax = function() {
       `colnames<-`(c("tax_id","tax_name","species","genus","family","order","class","phylum","kingdom","superkingdom"))
 
     ncbi_tax_ids <- as.numeric(ncbi_tax_ids) %>% as_tibble() %>% `colnames<-`(c("query_tax_id"))
-
     mapped <- ranked_lineage %>%
       right_join(blast_table , by = c("tax_id" = "taxonomy"))
 
-    d = unite(mapped, "taxonomy", rev(c(colnames(mapped[2:10]))), remove=TRUE, sep = ";")
-    df = subset(d, select=c(3:14,2,15:length(d)))
-    df$taxonomy = gsub("NA;", "", df$taxonomy)
+    d <- unite(mapped, "taxonomy", rev(c(colnames(mapped[2:10]))), remove=TRUE, sep = ";")
+    df <- subset(d, select=c(3:14,2,15:length(d)))
+    df$taxonomy <- gsub("NA;", "", df$taxonomy)
 
     fwrite(df, file = snakemake@output[[1]], na="")
 }

15c2375bb3c3b79b7722ea1c3f26a2b3ca70f0d1
Daniela Beisser <daniela.beisser@uni-due.de>
2019-08-20 13:45:01 +0200

    fixed ampliconduo filtering

--- scripts/filtering.py
+++ scripts/filtering.py
@@ -1,15 +1,20 @@
 import pandas as pd
 import tables
+import numpy as np
 
 df = pd.read_hdf(str(snakemake.input), 'df')
 
 if snakemake.params.filter_method == "split_sample":
-    # If more samples contain a value for a sequence than the unique list of
-    # samples without unit identifier, than the sequence is present in at least
-    # one _A, _B combination
-    uniq_samples =  len(set(sample[:-2] for sample in list(df.columns)))
-    df_filtered = df[(df >= 1).sum(axis=1) > uniq_samples]
-    df_filtered_out = df[(df >= 1).sum(axis=1) <= uniq_samples]
+    samples =  set(map(lambda x: x.split("_")[0], list(df.columns)))
+    for sample in samples:
+        pair = [ sample+'_A', sample+'_B']
+        # replace value by zero where XOR is True for A and B pair of samples,
+        # since where() replace False positions, replace the invert of xor
+        df[pair] = df[pair].where(np.invert(np.logical_xor(df[pair[0]]>=1, df[pair[1]]>=1)),0)
+    # finally remove all empty rows
+    selected_rows = df.sum(axis=1)>0
+    df_filtered = df[selected_rows]
+    df_filtered_out = df[np.invert(selected_rows)]
 elif snakemake.params.filter_method == "not_split":
     df_filtered = df[(df > snakemake.params.cutoff).all(1)]
     df_filtered_out = df[(df <= snakemake.params.cutoff).all(1)]

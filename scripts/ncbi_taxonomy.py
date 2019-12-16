3cd7d6fa9de3408ca3bc587283f02ce49951a9f0
Michael Schwarz <schwarzk@mathematik.uni-marburg.de>
2019-12-16 10:40:20 +0100

    added option to drop undesired classes from blast results

--- scripts/ncbi_taxonomy.py
+++ scripts/ncbi_taxonomy.py
@@ -24,16 +24,33 @@ def split2(char, string):
     else:
         return ["NaN"]
 
-querynames=list(set(blast_df.index.values))
+
+def create_filter_set():
+    res_set = set()
+    for f in snakemake.params.drop_tax_classes.split(","):
+        f = f.strip()
+        if f.isdigit():
+            res_set.add(int(f))
+        else:
+            # f = f.replace("*", ".*")
+            tmp = tax_df[tax_df.tax_lineage.str.contains(f, na=False)]
+            res_set.update(tmp.index)
+    return res_set
+
+querynames = list(set(blast_df.index.values))
 
 header = True
+f_set = create_filter_set()
 for i in querynames:
     blast_res = blast_df.loc[i,:] # blast results
     if blast_res.ndim == 1: # case only 1 blast hit
         blast_res = blast_res.to_frame().T
+    blast_res = blast_res[~blast_res['taxonomy'].isin(f_set)] # remove blast results with blacklisted taxonomy
     tax_res = tax_df.loc[blast_res['taxonomy'],:] # taxonomy for result
     tax_res = tax_res.set_index(blast_res.index)
     blast_res = pd.concat([blast_res, tax_res], axis=1)
+    if blast_res.empty:
+        continue
     blast_res["tax_level"] = blast_res.apply(lambda row: math.log(min(len(split2("; ", row['tax_lineage'])),6)), axis=1) # max length 6, log
     blast_res["len_val"] = blast_res.apply(lambda row: math.log(row['qlen']+1), axis=1)
     blast_res["Mscore"] = blast_res.apply(lambda row: row['pident']*row['tax_level']*row['len_val'], axis=1)
--- scripts/silva_taxonomy.py
+++ scripts/silva_taxonomy.py
@@ -16,13 +16,30 @@ taxids = list(set(blast_df["dbseqid"]))
 tax_df = pd.read_hdf(snakemake.input["lineage"], 'df')
 tax_df = tax_df.loc[taxids, :]
 
+
+def create_filter_set():
+    res_set = set()
+    for f in snakemake.params.drop_tax_classes.split(","):
+        f = f.strip()
+        if any(tax_df.index == f):
+            res_set.add(f)
+        else:
+            tmp = tax_df[tax_df.taxonomy.str.contains(f, na=False)]
+            res_set.update(tmp.index)
+    return res_set
+
+
 querynames=list(set(blast_df.index.values))
 
 header = True
+f_set = create_filter_set()
 for i in querynames:
     blast_res = blast_df.loc[i,:] # blast results
     if blast_res.ndim == 1: # case only 1 blast hit
         blast_res = blast_res.to_frame().T
+    blast_res = blast_res[~blast_res['dbseqid'].isin(f_set)] # remove blast results with blacklisted taxonomy
+    if blast_res.empty:
+        continue
     tax_res = tax_df.loc[blast_res['dbseqid'],:] # taxonomy for result
     tax_res = tax_res.set_index(blast_res.index)
     blast_res = pd.concat([blast_res, tax_res], axis=1)

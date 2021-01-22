5962405be6d0d9c7515d3189d1158a3554c09ca1
Marius Welzel <marius.welzel@gmx.de>
2021-01-22 10:24:13 +0100

    bugfixed pipeline.sh

--- pipeline.sh
+++ pipeline.sh
@@ -6,11 +6,11 @@ read varname
 env_loc=$(conda info --base)/etc/profile.d/conda.sh
 
 source $env_loc
-conda activate snakemake
+conda activate natrix
 #python demultiplexing.py "$varname"
 
 varname+=".yaml"
 cores=$(grep "cores : " $varname | awk '{print $3}')
 python create_dataframe.py "$varname"
 
-screen -S $varname bash -c "source $env_loc;conda activate snakemake;snakemake --use-conda --cores $cores --configfile $varname; exec sh"
+screen -S $varname bash -c "source $env_loc;conda activate natrix;snakemake --use-conda --cores $cores --configfile $varname; exec sh"

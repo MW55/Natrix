adc3e5e0e7e6b854930c2f98d99452dbc7dbfa48
Sandra Clemens <s.clemens@lum-software.de>
2021-05-10 21:32:50 +0200

    fix splitting (ncbi)

--- scripts/split_taxonomy.py
+++ scripts/split_taxonomy.py
@@ -4,14 +4,15 @@ import pandas as pd
 
 skip_columns = ['sequences', 'qlen', 'length', 'pident', 'mismatch', 'qstart', 'qend', 'sstart', 'send', 'gaps',
                 'evalue', 'seqid', 'Unnamed: 0']
-blast_table = pd.read_csv(snakemake.input[0], index_col='taxonomy', usecols=lambda x: x not in skip_columns)
-
-tax_keys = blast_table['tax_key'] if snakemake.params.database == 'NCBI' else None
-tax_keys = tax_keys.T
+blast_table = pd.read_csv(snakemake.input[0],  index_col='taxonomy', usecols=lambda x: x not in skip_columns)
+if snakemake.params.database == 'NCBI':
+    tax_keys = blast_table[['tax_key']]
+    tax_keys = tax_keys.groupby(tax_keys.index).first()
+    blast_table = blast_table.drop(['tax_key'], axis=1)
 blast_table = blast_table.astype('int64')
 blast_table = blast_table.rename(columns=lambda x: x.split('-')[0])
 blast_table = blast_table.T
-
+blast_table = blast_table.groupby(by=blast_table.columns, axis=1).sum()
 
 ranks = ['phylum','class','order','family','genus','species']
 
@@ -20,16 +21,16 @@ def rindex(lst, value):
     return len(lst) - operator.indexOf(reversed(lst), value) if value in lst else -1
 
 def split_taxonomy(x,rank):
-    key = tax_keys.at['tax_key',x]
+    key = tax_keys.at[x,'tax_key']
     index = rindex(key.split(';'),rank)
-    return x if index == -1 else x[:index]
+    return x if index == -1 else ';'.join(x.split(';')[:index])
 
 for o in reversed(snakemake.output):
     rank = ranks.pop()
     if(snakemake.params.database == 'NCBI'):
         blast_table = blast_table.rename(columns=lambda x: split_taxonomy(x, rank))
-        tax_keys = tax_keys.rename(columns=lambda x: split_taxonomy(x, rank))
-        tax_keys = tax_keys.groupby(by=tax_keys.columns, axis=1).first()
+        tax_keys = tax_keys.rename(index=lambda x: split_taxonomy(x, rank))
+        tax_keys = tax_keys.groupby(tax_keys.index).first()
 
     blast_table = blast_table.groupby(by=blast_table.columns, axis=1).sum()
     blast_table.to_csv(o)
